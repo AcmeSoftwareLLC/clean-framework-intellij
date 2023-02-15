1cfc1c6bf44254013bcd374bc523f57f3c1ce872
sarbagyastha <sarbagyastha@gmail.com>
2023-02-15 16:19:23 +0545

    minor fix

--- src/main/kotlin/com/acmesoftware/cleanframework/actions/SetupProjectAction.kt
+++ src/main/kotlin/com/acmesoftware/cleanframework/actions/SetupProjectAction.kt
@@ -1,5 +1,6 @@
 package com.acmesoftware.cleanframework.actions
 
+import com.google.common.base.CaseFormat
 import com.intellij.openapi.actionSystem.*
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.command.CommandProcessor
@@ -20,6 +21,7 @@ class SetupProjectAction : AnAction() {
         val pubspecFile = PubspecYamlUtil.findPubspecYamlFile(project!!, directory.virtualFile)
         val libDir = pubspecFile!!.parent.findChild(PubspecYamlUtil.LIB_DIR_NAME)!!
         val packageName = PubspecYamlUtil.getDartProjectName(pubspecFile)!!
+        val packageNamePascal = CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, packageName)
 
         ApplicationManager.getApplication().runWriteAction {
             val runnable = Runnable {
@@ -29,11 +31,6 @@ class SetupProjectAction : AnAction() {
                 var providersDirectory = libDirectory.findSubdirectory("providers")
                 if (providersDirectory == null) {
                     providersDirectory = libDirectory.createSubdirectory("providers")
-                }
-
-                val featuresDirectory = libDirectory.findSubdirectory("features")
-                if (featuresDirectory == null) {
-                    libDirectory.createSubdirectory("features")
                 }
 
                 val providerFiles = listOf(
@@ -57,6 +54,115 @@ class SetupProjectAction : AnAction() {
 
                     val file = fileFactory.createFileFromText("providers.dart", DartLanguage.INSTANCE, content)
                     libDirectory.add(file)
+                }
+
+                var routingDirectory = libDirectory.findSubdirectory("routing")
+                if (routingDirectory == null) {
+                    routingDirectory = libDirectory.createSubdirectory("routing")
+                }
+
+                val routerFileName = "${packageName}_router.dart"
+                val routerFile = libDirectory.findFile(routerFileName)
+                if (routerFile == null) {
+                    val content = """
+                        import 'package:clean_framework_router/clean_framework_router.dart';
+                        import 'package:$packageName/routing/src/routes.dart';
+                        import 'package:flutter/material.dart';
+
+                        class ${packageNamePascal}Router extends AppRouter<Routes> {
+                          @override
+                          RouterConfiguration configureRouter() {
+                            return RouterConfiguration(
+                              routes: [
+                                AppRoute(
+                                  route: Routes.home,
+                                  builder: (_, __) => const Placeholder(),
+                                ),
+                              ],
+                            );
+                          }
+                        }
+                    """.trimIndent()
+                    val file = fileFactory.createFileFromText(routerFileName, DartLanguage.INSTANCE, content)
+                    routingDirectory.add(file)
+                }
+
+                val routesFileName = "routes.dart"
+                val routesFile = libDirectory.findFile(routesFileName)
+                if (routesFile == null) {
+                    val content = """
+                        import 'package:clean_framework_router/clean_framework_router.dart';
+
+                        enum Routes with RoutesMixin {
+                          home('/');
+
+                          const Routes(this.path);
+
+                          @override
+                          final String path;
+                        }
+                    """.trimIndent()
+                    val file = fileFactory.createFileFromText(routesFileName, DartLanguage.INSTANCE, content)
+                    routingDirectory.add(file)
+                }
+
+                val routingFile = libDirectory.findFile("routing.dart")
+                if (routingFile == null) {
+                    val content = """
+                        export 'package:$packageName/routing/$routerFileName';
+                        export 'package:$packageName/routing/routes.dart';
+                    """.trimIndent()
+
+                    val file = fileFactory.createFileFromText("routing.dart", DartLanguage.INSTANCE, content)
+                    libDirectory.add(file)
+                }
+
+                var appDirectory = libDirectory.findSubdirectory("app")
+                if (appDirectory == null) {
+                    appDirectory = libDirectory.createSubdirectory("app")
+                }
+
+                val appFileName = "${packageName}_app.dart"
+                val appFile = libDirectory.findFile(appFileName)
+                if (appFile == null) {
+                    val content = """
+                        import 'package:clean_framework/clean_framework.dart';
+                        import 'package:clean_framework_router/clean_framework_router.dart';
+                        import 'package:flutter/material.dart';
+                        import 'package:$packageName/routing.dart';
+
+                        class ${packageNamePascal}App extends StatelessWidget {
+                          const ${packageNamePascal}App({super.key});
+
+                          @override
+                          Widget build(BuildContext context) {
+                            return AppProviderScope(
+                              child: AppRouterScope(
+                                create: ${packageNamePascal}Router.new,
+                                builder: (context) {
+                                  return MaterialApp.router(
+                                    title: '$packageName',
+                                    theme: ThemeData(
+                                      colorSchemeSeed: Colors.blue,
+                                      useMaterial3: true,
+                                    ),
+                                    routerConfig: context.router.config,
+                                  );
+                                },
+                              ),
+                            );
+                          }
+                        }
+
+                    """.trimIndent()
+
+                    val file = fileFactory.createFileFromText(appFileName, DartLanguage.INSTANCE, content)
+                    appDirectory.add(file)
+                }
+
+                val featuresDirectory = libDirectory.findSubdirectory("features")
+                if (featuresDirectory == null) {
+                    libDirectory.createSubdirectory("features")
                 }
             }
 
