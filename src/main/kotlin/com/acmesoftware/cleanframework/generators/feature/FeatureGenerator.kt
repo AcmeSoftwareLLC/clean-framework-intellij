a1923264a951b15a737ab2f145ba69b8e0406d78
sarbagyastha <sarbagyastha@gmail.com>
2023-02-15 16:40:47 +0545

    added template class

--- src/main/kotlin/com/acmesoftware/cleanframework/generators/feature/FeatureGenerator.kt
+++ src/main/kotlin/com/acmesoftware/cleanframework/generators/feature/FeatureGenerator.kt
@@ -1,23 +1,30 @@
 package com.acmesoftware.cleanframework.generators.feature
 
-import com.acmesoftware.cleanframework.utils.TemplateUtils
+import com.acmesoftware.cleanframework.utils.Template
 import com.google.common.base.CaseFormat
-import org.apache.commons.lang.text.StrSubstitutor
 
-abstract class FeatureGenerator(packageName: String, val featureName: String, val layer: String, private val templateName: String) {
-    private val template: String = TemplateUtils.read("feature/$layer/$templateName.dart.cf")
+abstract class FeatureGenerator(
+    private val packageName: String,
+    val featureName: String,
+    val layer: String,
+    private val templateName: String
+) {
+    private val template = Template("feature/$layer/$templateName.dart.cf")
 
-    private val values: HashMap<String, String> = hashMapOf(
-        "package_name" to packageName,
-        "feature_name" to featureName,
-        "feature_name_snake" to CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, featureName),
-        "feature_name_camel" to CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_CAMEL, featureName),
-    )
+    private val featureNameSnake = CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, featureName)
+    private val featureNameCamel = CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_CAMEL, featureName)
 
-    val fileName get(): String = "${values["feature_name_snake"]}_$templateName.dart"
+
+    val fileName get(): String = "${featureNameSnake}_$templateName.dart"
 
     fun generate(): String {
-        val substitutor = StrSubstitutor(values, "{{", "}}", '\\')
-        return substitutor.replace(template)
+        return template.fill(
+            hashMapOf(
+                "package_name" to packageName,
+                "feature_name" to featureName,
+                "feature_name_snake" to featureNameSnake,
+                "feature_name_camel" to featureNameCamel,
+            )
+        )
     }
 }
--- src/main/kotlin/com/acmesoftware/cleanframework/utils/Template.kt
+++ src/main/kotlin/com/acmesoftware/cleanframework/utils/Template.kt
@@ -1,19 +1,27 @@
 package com.acmesoftware.cleanframework.utils
 
 import com.google.common.io.CharStreams
+import org.apache.commons.lang.text.StrSubstitutor
 import java.io.InputStreamReader
 
-object TemplateUtils {
-    fun read( templatePath: String): String {
+class Template(templatePath: String) {
+    private val template: String
+
+    init {
         val resource = "/templates/$templatePath"
 
         try {
             val resourceStream = this::class.java.getResourceAsStream(resource)!!
-            return CharStreams.toString(InputStreamReader(resourceStream, Charsets.UTF_8))
+            template = CharStreams.toString(InputStreamReader(resourceStream, Charsets.UTF_8))
         } catch (e: Exception) {
             e.printStackTrace()
             throw TemplateNotFoundException(resource)
         }
+    }
+
+    fun fill(values: HashMap<String, String>): String {
+        val substitutor = StrSubstitutor(values, "{{", "}}", '\\')
+        return substitutor.replace(template)
     }
 }
 
