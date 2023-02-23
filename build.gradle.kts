cdd9f5ab723e49ef0023ece7f56a4255e8c96db3
sarbagyastha <sarbagyastha@gmail.com>
2023-02-23 16:29:45 +0545

    pull changelog and plugin description from repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,46 +1,70 @@
+import org.jetbrains.changelog.markdownToHTML
+
+fun properties(key: String) = project.findProperty(key).toString()
+
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "1.8.0"
     id("org.jetbrains.intellij") version "1.13.0"
+    id("org.jetbrains.changelog") version "2.0.0"
 }
 
-fun properties(key: String) = project.findProperty(key).toString()
 
-group = properties("domain")
+group = properties("pluginGroup")
 version = properties("pluginVersion")
 
 repositories {
     mavenCentral()
 }
 
+kotlin {
+    jvmToolchain(11)
+}
+
 intellij {
     pluginName.set(properties("pluginName"))
-    version.set(properties("baseVersion"))
-    type.set("IC") // Target IDE Platform
+    version.set(properties("platformVersion"))
+    type.set(properties("platformType")) // Target IDE Platform
 
-    plugins.set(
-        listOf(
-            "java",
-            "Kotlin",
-            "io.flutter:${properties("flutterVersion")}",
-            "Dart:${properties("dartVersion")}"
-        )
-    )
+    plugins.set(properties("platformPlugins").split(',').map(String::trim).filter(String::isNotEmpty))
+}
+
+changelog {
+    groups.set(emptyList())
+    repositoryUrl.set(properties("pluginRepositoryUrl"))
 }
 
 tasks {
-    // Set the JVM compatibility versions
-    withType<JavaCompile> {
-        sourceCompatibility = "11"
-        targetCompatibility = "11"
-    }
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.jvmTarget = "11"
+    wrapper {
+        gradleVersion = properties("gradleVersion")
     }
 
     patchPluginXml {
+        version.set(properties("pluginVersion"))
         sinceBuild.set(properties("pluginSinceBuild"))
         untilBuild.set(properties("pluginUntilBuild"))
+
+        pluginDescription.set(
+            file("README.md").readText().lines().run {
+                val start = "<!-- Plugin description -->"
+                val end = "<!-- Plugin description end -->"
+
+                if (!containsAll(listOf(start, end))) {
+                    throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
+                }
+                subList(indexOf(start) + 1, indexOf(end))
+            }.joinToString("\n").let { markdownToHTML(it) }
+        )
+
+        changeNotes.set(provider {
+            with(changelog) {
+                renderItem(
+                    getOrNull(properties("pluginVersion"))
+                        ?: runCatching { getLatest() }.getOrElse { getUnreleased() },
+                    org.jetbrains.changelog.Changelog.OutputType.HTML,
+                )
+            }
+        })
     }
 
     signPlugin {
